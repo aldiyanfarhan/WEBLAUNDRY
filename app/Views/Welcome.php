7e9aa0a0aaa075820b345a49b7dde3c01830befe
aldiyanfarhan <aldiyanfarhan@gmail.com>
2021-09-30 01:27:59 +0700

    Big Update
    Anjiing bikin view doang lama bat

--- app/Views/Welcome.php
+++ app/Views/Welcome.php
@@ -1,60 +1,311 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+  <!-- basic -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- mobile metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+  <!-- site metas -->
+  <title>meticulous</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- style css -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- Responsive-->
+  <link rel="stylesheet" href="css/responsive.css">
+  <!-- fevicon -->
+  <link rel="icon" href="images/fevicon.png" type="image/gif" />
+  <!-- Scrollbar Custom CSS -->
+  <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+  <!-- Tweaks for older IEs-->
+  <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+</head>
+<!-- body -->
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+<body class="main-layout">
+  <!-- loader  -->
+  <!-- <div class="loader_bg">
+    <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+  </div> -->
+  <!-- end loader -->
 
-  <!-- Navbar section -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Liquid Laundry</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
+  <!-- header -->
+  <header>
+    <!-- header inner -->
+    <div class="header">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+            <div class="full">
+              <div class="center-desk">
+                <div class="logo">
+                  <a href="index.html"><img src="images/logo.png" alt="#" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+            <nav class="navigation navbar navbar-expand-md navbar-dark ">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarsExample04">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#we_do">We Do</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                  </li>
+                </ul>
+                <div class="sign_btn"><a href="#">Sign in</a></div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </header>
+  <!-- end header inner -->
+  <!-- end header -->
+
+  <!-- banner -->
+  <section class="banner_main">
+    <div class="container">
+      <div class="row d_flex">
+        <div class="col-md-4">
+          <div class="text-bg">
+            <h1>WE <br>
+              WILL <br>
+              bettter<br>
+              Clean <br>
+              <span class="black">Services </span>
+            </h1>
+            <a href="#contact">Contact Us</a>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="text-img">
+            <figure><img src="images/clean.png" alt="#" /></figure>
+          </div>
+          <!-- <div class="text-img1">
+            <figure><img src="images/clean2.png" alt="#" /></figure>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end banner -->
+
+  <!-- we_do -->
+  <div id="we_do" class="we_do">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="titlepage">
+            <h2>What we do?</h2>
+            <span></span>
+            <strong></strong>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="we_do_box">
+            <i><img src="images/antri.png" alt="#" /></i>
+            <h4>House clean</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="we_do_box">
+            <i><img src="images/mencuci.png" alt="#" /></i>
+            <h4>office Clean</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="we_do_box">
+            <i><img src="images/pengeringan.png" alt="#" /></i>
+            <h4>Factory clean</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="we_do_box">
+            <i><img src="images/setrika.png" alt="#" /></i>
+            <h4>Factory clean</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</nav>
+  <!-- end we_do -->
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!-- Info section start -->
+  <div class="row" style="background-color: #f5b707;">
+    <div id="taxis" class="taxis_section layout_padding">
+      <div class="container">
+        <h1 class="our_text">TRACKING <span style="color: #f4db31;">INFO</span></h1>
+        <div class="taxis_section_2">
+          <div class="row">
+            <div class="col-sm-4 cornerinfo">
+              <div class="taxi_main">
+                <div class="round_1">01</div>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
+                <div class="input-group">
+                  <input type="search" class="form-control rounded" placeholder="Masukan Kode Laundry" aria-label="Search" aria-describedby="search-addon" />
+                  <button type="button" class="btn btn-outline-primary">Cari</button>
+                </div>
+                <br><br>
+
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="taxi_main">
+                <div class="round_1">02</div>
+                <h2 class="carol_text">CAR 2</h2>
+                <p class="reader_text">act that a reader will be<br>
+                  distracted </p>
+                <div class="images_2"><img src="images/img-2.png"></a></div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="taxi_main">
+                <div class="round_1">03</div>
+                <h2 class="carol_text">CAR 3</h2>
+                <p class="reader_text">act that a reader will be<br>
+                  distracted </p>
+                <div class="images_2"><img src="images/img-2.png"></a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--  Info section end -->
+
+  <!-- contact -->
+  <div class="row">
+
+  </div>
+
+  <div id="contact" class="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="titlepage">
+            <br><br>
+            <h2><u>Contact Us</u></h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 ">
+          <form class="main_form">
+            <div class="row">
+              <div class="col-md-12 ">
+                <input class="contactus" placeholder="Nmae" type="text" name="Nmae">
+              </div>
+              <div class="col-md-6">
+                <input class="contactus" placeholder="Phone Number" type="text" name=" Phone Number">
+              </div>
+              <div class="col-md-6">
+                <input class="contactus" placeholder="Services" type="text" name="Services">
+              </div>
+              <div class="col-md-12">
+                <input class="contactus" placeholder="Email " type="text" name="Email ">
+              </div>
+              <div class="col-md-12">
+                <textarea class="textarea" placeholder="Message" type="text" name="Message">Message</textarea>
+              </div>
+              <div class="col-sm-12">
+                <button class="send_btn">Send</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end contact -->
+  <!-- asked -->
+
+  <div class="asked">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <br><br>
+          <div class="titlepage">
+            <h2>Frequently Asked Questions</h2>
+            <span></span>
+            <strong></strong>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="some">
+            <h3>Where can I get some?</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed </p>
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="some">
+            <h3>How to do clean office</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed </p>
+            <!-- <a class=" read_more" href="#">Read More</a> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end asked -->
+  <!--  footer -->
+
+  <section class="footer">
+    <p class="credits">
+      Copyright @2021 Rafly Ilham Suyudi</a>
+    </p>
+    <ul class="footericons">
+      <li><a target="_blank" href="#"><i class="fa fa-facebook"></i></a></li>
+      <li><a target="_blank" href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a target="_blank" href="#"><i class="fa fa-google-plus"></a></i></li>
+      <li><a target="_blank" href="#"><i class="fa fa-youtube"></i></a></li>
+      <li><a target="_blank" href="#"><i class="fa fa-soundcloud"></i></a></li>
+      <li><a target="_blank" href="#"><i class="fa fa-apple"></i></a></li>
+      <li><a target="_blank" href="#"><i class="fa fa-amazon"></i></a></li>
+      <li><a target="_blank" href="#"><i class="fa fa-vimeo"></i></a></li>
+    </ul>
+  </section>
+
+  <!-- end footer -->
+  <!-- Javascript files-->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/jquery-3.0.0.min.js"></script>
+  <script src="js/plugin.js"></script>
+  <!-- sidebar -->
+  <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+  <script src="js/custom.js"></script>
+  <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+</body>
+
 </html>
